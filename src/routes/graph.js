423c9499b1b5e1e1d6e6126af37ed748995a1980
Jacob Bare <jacob@limit0.io>
2018-02-28 14:52:17 -0600

    Add no cache to graph api router

--- src/routes/graph.js
+++ src/routes/graph.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 const { graphqlExpress } = require('apollo-server-express');
 const schema = require('../graph/schema');
 
 const router = Router();
 
 router.use(
+  helmet.noCache(),
   bodyParser.json(),
   graphqlExpress(() => ({
     schema,
