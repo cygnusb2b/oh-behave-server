c5f4c8024c040686a42874e5b69059aa03da78f2
Jacob Bare <jacob@limit0.io>
2018-06-26 09:05:54 -0500

    Apply CORs to the main express app

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
 const passport = require('passport');
+
 const authStrategies = require('./auth-strategies');
 const routes = require('./routes');
 
 passport.use(authStrategies.bearer);
 
 const app = express();
+const CORS = cors();
 
 app.use(passport.initialize());
 app.use(helmet());
+
+app.use(CORS);
+app.options('*', CORS);
 
 routes(app);
 
